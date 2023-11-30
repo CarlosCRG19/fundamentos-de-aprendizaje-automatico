33262caee6dca5de3382d02851a94cab3fa5d6f0
HuffleDK <davidkaack@gmx.de>
2023-11-30 20:42:22 +0100

    Logica de evalua fitness y seleccion progenitores hecha y testeada. Falta codificacion para adaptar a formato

--- FAAP4_1461_6/AlgoritmoGenetico.py
+++ FAAP4_1461_6/AlgoritmoGenetico.py
@@ -3,6 +3,7 @@ from typing import List
 import numpy as np
 import pandas as pd
 from Clasificador import Clasificador
+from random import choices
 
 
 class Individuo:
@@ -57,3 +58,53 @@ class AlgoritmoGenetico(Clasificador):
                 codigo[i_valor] = 1
 
                 self.codificacion[i_atributo][valor] = codigo
+
+
+    # Esta parte necesesita la codificacion terminada
+    def _seleccion_progenitores(self, datos: pd.DataFrame, individuos: List[Individuo]) -> List[float]:
+        
+        lista_fitness = [self._evalua_fitness(datos, individuo) for individuo in individuos]
+        ruleta = [fitness/sum(lista_fitness) for fitness in lista_fitness]
+
+        cantidad_progenitores = round(0.8*len(individuos))
+        seleccion = choices(individuos, weights=ruleta, k = cantidad_progenitores)
+        return seleccion
+
+
+    def _evalua_fitness(self, datos: pd.DataFrame, individuo: Individuo) -> float:
+
+        clasificaciones = []
+
+        # clasifica todos los datos con individuo
+        for _, dato in datos.iterrows():
+            # Llama _fitness_regla con cada regla y tiene cuenta de las clasificaciones correctas
+            cada_regla = [True if self._fitness_regla(dato, regla) else False for regla in individuo.reglas]
+
+            # Si la mayoria de reglas clasifican bien, el dato ha sido clasificado correctamente
+            if sum(cada_regla) > np.floor(len(cada_regla)/2):
+                clasificaciones.append(1)
+            clasificaciones.append(0)
+        
+        # return la precision
+        return sum(clasificaciones) / len(clasificaciones)
+
+    def _fitness_regla(self, dato: pd.Series, regla: np.ndarray) -> bool:
+        
+        # crea una lista unidimensional de los bits
+        dato = dato.tolist().ravel()
+        indices = [i for i, x in enumerate(dato[:-1]) if x == 1]
+        # compara todos atributos con predicciones
+        # Ojo: Tambien compara las clases
+        cada_atributo = [True if regla[i] == 1 else False for i in indices]
+
+        # Si la regla reconoce todos atributos tambien tiene que predecir la clase correcta
+        # Si la regla no reconoce todos atributos tiene que predecir la clase contraria para clasificar el dato bien
+        if (
+            sum(cada_atributo) < len(cada_atributo) and dato[-1] != regla[-1]
+            or all(cada_atributo) and dato[-1] == regla[-1]
+            ):
+            return True
+        return False
+
+
+
