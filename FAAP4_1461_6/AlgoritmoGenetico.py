ea7da72756b319dc5b2a38691a0682e757019327
CarlosCRG19 <carloscrg19@gmail.com>
2023-12-05 09:11:08 -0600

    primer intento hasta clasificacion con un individuo

--- FAAP4_1461_6/AlgoritmoGenetico.py
+++ FAAP4_1461_6/AlgoritmoGenetico.py
@@ -1,20 +1,78 @@
-from typing import List
+from random import choices
+from typing import Dict, List, Tuple
 
 import numpy as np
 import pandas as pd
+
 from Clasificador import Clasificador
-from random import choices
 
 
 class Individuo:
-    def __init__(self, reglas):
+    def __init__(self, reglas: np.ndarray):
         self.reglas = reglas
 
+    @staticmethod
     def crea_con_reglas_aleatorias(max_reglas: int, longitud_reglas: int):
-        reglas = np.random.randint(
-            2, size=(np.random.randint(1, max_reglas), longitud_reglas)
-        )
+        while True:
+            reglas = np.random.randint(
+                2, size=(np.random.randint(1, max_reglas), longitud_reglas)
+            )
+            # Verificar si alguna fila tiene solo 0s o solo 1s
+            if not np.any(np.all(reglas == 0, axis=1)) and not np.any(
+                np.all(reglas == 1, axis=1)
+            ):
+                break
+
         return Individuo(reglas)
+
+
+# Revisar con el profesor si debemos de utilizar Datos
+
+
+class CodificadorBinario:
+    def __init__(self, datos: pd.DataFrame):
+        self._n_bits, self._codificacion = self._init_codificacion(datos)
+
+    def _init_codificacion(
+        self, datos: pd.DataFrame
+    ) -> Tuple[int, Dict[str, Dict[str, List[int]]]]:
+        columnas = datos.columns
+        codificacion = {}
+        n_bits = 0
+
+        for columna in columnas[:-1]:
+            valores = sorted(datos[columna].astype(str).unique())
+            codificacion[columna] = {}
+
+            for i, valor in enumerate(valores):
+                codigo = [0] * len(valores)
+                codigo[i] = 1
+
+                codificacion[columna][valor] = codigo
+
+            n_bits += len(valores)
+
+        # Columna de clase, tiene una codificación con un solo bit
+        codificacion[columnas[-1]] = {"0": [0], "1": [1]}
+        n_bits += 1
+
+        return n_bits, codificacion
+
+    def codifica_datos(self, datos: pd.DataFrame) -> np.ndarray:
+        filas_codificadas = []
+
+        for _, fila in datos.iterrows():
+            fila_codificada = []
+
+            for columna, valor in fila.items():
+                fila_codificada.extend(self._codificacion[columna][str(valor)])
+
+            filas_codificadas.append(fila_codificada)
+
+        return np.array(filas_codificadas)
+
+    def n_bits(self) -> int:
+        return self._n_bits
 
 
 class AlgoritmoGenetico(Clasificador):
@@ -24,72 +82,75 @@ class AlgoritmoGenetico(Clasificador):
         self.max_reglas = max_reglas
         self.poblacion = []
 
-    def entrenamiento(self, datosTrain, nominalAtributos, diccionario):
-        self._crea_codificacion(datosTrain, nominalAtributos)
+    def entrenamiento(
+        self, datosTrain: pd.DataFrame, nominalAtributos: List[bool], diccionario: Dict
+    ):
+        # Crea codificacion -> esto podria ser a traves de diccionario
+        self.codificador = CodificadorBinario(datosTrain)
         # Crear primera generacion
-        # Obtener tamanio de reglas
-        longitud_reglas = self._longitud_reglas(datosTrain)
-        # Generar `tamano_poblacion` individuos con reglas aleatorias
-        for _ in range(self.tamano_poblacion):
-            self.poblacion.append(
-                Individuo.crea_con_reglas_aleatorias(self.max_reglas, longitud_reglas)
-            )
+        self.poblacion = self._crea_primera_generacion()
+
+        datos_codificados = self.codificador.codifica_datos(datosTrain)
 
         # for _ in range(epocas):
+        # calcular fitness de la poblacion
+        # utilizar elitismo
+        # seleccionar padres
+        # Crossovers => cruzar los padres
+        # Mutaciones => mutar los padres para generar descendientes
+        # Sobrevivientes => seleccionar solo los mejores descendientes
+        # actualizar poblacion
 
-    def _longitud_reglas(self, datos: pd.DataFrame) -> int:
-        X = datos.iloc[:, :-1]
-        return X.nunique().sum() + 1
+        # calcular fitness de la poblacion final
+        # calcular mejor solucion (mejor individuo)
 
-    def _codificar_datos(self, datos: pd.DataFrame) -> np.ndarray:
-        if self.codificacion is None:
-            self.crea_codificacion(datos)
-        pass
+    def _crea_primera_generacion(self, datosTrain: pd.DataFrame) -> List[Individuo]:
+        poblacion = []
 
-    def _crea_codificacion(self, datos: pd.DataFrame, nominalAtributos: List[bool]):
-        self.codificacion = [{}] * len(nominalAtributos)
+        # Generar `tamano_poblacion` individuos con reglas aleatorias
+        for _ in range(self.tamano_poblacion):
+            poblacion.append(
+                Individuo.crea_con_reglas_aleatorias(
+                    max_reglas=self.max_reglas,
+                    longitud_reglas=self.codificador.n_bits(),
+                )
+            )
 
-        for i_atributo, _ in enumerate(nominalAtributos):
-            valores = [str(valor) for valor in datos.iloc[:, i_atributo]].unique()
-            valores = sorted(valores)
-
-            for i_valor, valor in valores:
-                codigo = [0] * len(valores)
-                codigo[i_valor] = 1
-
-                self.codificacion[i_atributo][valor] = codigo
-
+        return poblacion
 
     # Esta parte necesesita la codificacion terminada
-    def _seleccion_progenitores(self, datos: pd.DataFrame, individuos: List[Individuo]) -> List[float]:
-        
-        lista_fitness = [self._evalua_fitness(datos, individuo) for individuo in individuos]
-        ruleta = [fitness/sum(lista_fitness) for fitness in lista_fitness]
+    def _seleccion_progenitores(
+        self, datos: pd.DataFrame, individuos: List[Individuo]
+    ) -> List[float]:
+        lista_fitness = [
+            self._evalua_fitness(datos, individuo) for individuo in individuos
+        ]
+        ruleta = [fitness / sum(lista_fitness) for fitness in lista_fitness]
 
-        cantidad_progenitores = round(0.8*len(individuos))
-        seleccion = choices(individuos, weights=ruleta, k = cantidad_progenitores)
+        cantidad_progenitores = round(0.8 * len(individuos))
+        seleccion = choices(individuos, weights=ruleta, k=cantidad_progenitores)
         return seleccion
 
-
     def _evalua_fitness(self, datos: pd.DataFrame, individuo: Individuo) -> float:
-
         clasificaciones = []
 
         # clasifica todos los datos con individuo
         for _, dato in datos.iterrows():
             # Llama _fitness_regla con cada regla y tiene cuenta de las clasificaciones correctas
-            cada_regla = [True if self._fitness_regla(dato, regla) else False for regla in individuo.reglas]
+            cada_regla = [
+                True if self._fitness_regla(dato, regla) else False
+                for regla in individuo.reglas
+            ]
 
             # Si la mayoria de reglas clasifican bien, el dato ha sido clasificado correctamente
-            if sum(cada_regla) > np.floor(len(cada_regla)/2):
+            if sum(cada_regla) > np.floor(len(cada_regla) / 2):
                 clasificaciones.append(1)
             clasificaciones.append(0)
-        
+
         # return la precision
         return sum(clasificaciones) / len(clasificaciones)
 
     def _fitness_regla(self, dato: pd.Series, regla: np.ndarray) -> bool:
-        
         # crea una lista unidimensional de los bits
         dato = dato.tolist().ravel()
         indices = [i for i, x in enumerate(dato[:-1]) if x == 1]
@@ -100,11 +161,10 @@ class AlgoritmoGenetico(Clasificador):
         # Si la regla reconoce todos atributos tambien tiene que predecir la clase correcta
         # Si la regla no reconoce todos atributos tiene que predecir la clase contraria para clasificar el dato bien
         if (
-            sum(cada_atributo) < len(cada_atributo) and dato[-1] != regla[-1]
-            or all(cada_atributo) and dato[-1] == regla[-1]
-            ):
+            sum(cada_atributo) < len(cada_atributo)
+            and dato[-1] != regla[-1]
+            or all(cada_atributo)
+            and dato[-1] == regla[-1]
+        ):
             return True
         return False
-
-
-
